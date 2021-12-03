6b548b3e54a0534ca8c29ce8c278a6ed8fb63e40
John Penning <3259324+johnpenning@users.noreply.github.com>
2021-12-03 15:10:39 -0800

    Create Xcode project

---
+++ GitIssues/GitIssues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitIssues
+//
+//  Created by John Penning on 12/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
